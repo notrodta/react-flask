7455087673d1043b1e9a867f0c5fbaf441d2d9e9
Rod Ta <rody101@gmail.com>
2022-04-01 00:37:43 -0500

    small refactor

--- clientapp/src/setupTests.ts
+++ clientapp/src/setupTests.ts
@@ -3,7 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { server } from './mocks/Server';
+import { server } from './mocks/ServerSetup';
 
 beforeAll(() =>
   server.listen({
