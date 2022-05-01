d8f111bde396ae1ea4547248be5cda386ed7903e
Rod Ta <rody101@gmail.com>
2022-05-01 15:32:25 -0400

    Using application factory for flask app

--- app.py
+++ app.py
@@ -9,35 +9,40 @@ from ma import ma
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-app = Flask(__name__)
-# CORS(app)
-# CORS(app, supports_credentials=True)
-# cors = CORS(app, resources={r"/*": {"origins": "*"}})
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-# cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
-# cors = CORS(app , resources={r"/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.secret_key = "jose"  # could do app.config['JWT_SECRET_KEY'] if we prefer
-api = Api(app)
+def create_app():
+    app = Flask(__name__)
+    # CORS(app)
+    # CORS(app, supports_credentials=True)
+    # cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    # cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+    # cors = CORS(app , resources={r"/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}})
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.secret_key = "jose"  # could do app.config['JWT_SECRET_KEY'] if we prefer
+    api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
 
-@app.errorhandler(ValidationError)
-def handle_marshmallow_validation(err):
-    return jsonify(err.messages), 400
+    @app.errorhandler(ValidationError)
+    def handle_marshmallow_validation(err):
+        return jsonify(err.messages), 400
 
 
-api.add_resource(Store, "/api/store/<string:name>")
-api.add_resource(StoreList, "/api/stores")
-api.add_resource(Item, "/api/item/<string:name>")
-api.add_resource(ItemList, "/api/items")
+    api.add_resource(Store, "/api/store/<string:name>")
+    api.add_resource(StoreList, "/api/stores")
+    api.add_resource(Item, "/api/item/<string:name>")
+    api.add_resource(ItemList, "/api/items")
+
+    return app
+
+app  = create_app()
 
 if __name__ == "__main__":
     db.init_app(app)
